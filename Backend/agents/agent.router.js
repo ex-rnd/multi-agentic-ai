44a4acddfd4b27e400720bbcf0699d414c97a455
James Kilonzo <jkilonzo2023@gmail.com>
2026-04-24 16:11:28 +0300

    Update: health.agent.js

--- Backend/agents/agent.router.js
+++ Backend/agents/agent.router.js
@@ -2,6 +2,7 @@
 
 import axios from "axios";
 import dotenv from "dotenv";
+dotenv.config();
 
 export const OLLAMA_URL =  process.env.OLLAMA_URL;
 export const GEN_MODEL =  process.env.GEN_MODEL;
---
+++ Backend/agents/health.agent.js
@@ -0,0 +1,55 @@
+// File Location: ./Backend/agents//health.agent.js 
+
+import handleHealthQuery from "agent.router.js";
+import { ollamaChat } from "../services/ollama";
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const OLLAMA_URL =  process.env.OLLAMA_URL;
+export const GEN_MODEL =  process.env.GEN_MODEL;
+
+export async function handleHealthQuery(message) {
+
+    const system = 
+    `
+    You are an "HealthInfo", a cautious health information assistant.
+    SCOPE:
+    - General health education: Symptoms, prevention, lifestyle, diet, treatment overviews.
+    - No diagnosis, prescriptions, or emergencies 
+    POLICY:
+    - If user asks outside health scope, say: "Sorry, I only provide general health information."
+    - Keep answers concise, clear, and actionable when possible.
+    - Include a brief disclaimer at the end: "(General info, not medical advice.)"
+    
+    `.trim();
+
+    const reply = await ollamaChat
+    (
+        [
+            {
+                role: "system", content: system
+            },
+            {
+                role: "user", content: message
+            },
+        ],
+        {
+            model: process.env.GEN_MODEL,
+            stream: false
+        }
+
+    );
+
+    return {
+        answer: (reply | "Sorry, I could help with only health related query"),
+        source: "health"
+    }
+
+
+
+
+}
+
+
+
