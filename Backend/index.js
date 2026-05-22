973b24b225a485a5c1b06b29de22a67dc9e512d3
James Kilonzo <jkilonzo2023@gmail.com>
2026-05-22 09:51:16 +0300

    Merge: Merged feature chatpage

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import chatRoute from './routes/chatRoute.js';
+import cors from "cors";
 
 dotenv.config()
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// Using CORS
+app.use(cors(
+    {
+        origin : '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type']
+    }
+))
 
 // Express
 app.use(express.json())
